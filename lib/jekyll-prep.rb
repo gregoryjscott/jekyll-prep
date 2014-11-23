09137d70e4404dd84c4b04f1a5aa03d14319ad18
Greg Scott <i@gregoryjscott.com>
2014-11-23 11:38:58 -0600

    Remove stupid shit

--- lib/jekyll-prep.rb
+++ lib/jekyll-prep.rb
@@ -2,49 +2,26 @@ require 'find'
 require 'jekyll'
 
 module Jekyll
+
   class Prep < Jekyll::Generator
+
     def generate(site)
-      files = get_data_files
-      files.each do |file|
-        data = YAML.load_file(file)
-        add_links_counts data
-        data = update_item_page_data site, file, data
-        update_list_page_data site, file, data unless file == '_tmp/content/_data/index.yml'
-      end
-    end
-
-    def get_data_files
-      files = []
-      puts Dir.pwd
-      Find.find('_tmp/content/_data') do |file|
-        if file=~/.yml$/
-          files << file
+      Dir.chdir(site.source) do
+        files = Dir['_data/**/*.yml']
+        files.each do |file|
+          data = YAML.load_file(file)
+          data = update_item_page_data site, file, data
+          update_list_page_data site, file, data unless file == '_data/index.yml'
         end
-      end
-      files
-    end
-
-    def add_links_counts(data)
-      add_links_count data, 'work'
-      add_links_count data, 'skills'
-      add_links_count data, 'contact'
-      add_links_count data, 'projects'
-      add_links_count data, 'languages'
-      add_links_count data, 'os'
-    end
-
-    def add_links_count(data, field)
-      unless data['links'].nil? or data['links'][field].nil?
-        data["#{field}_count"] = data['links'][field].size
       end
     end
 
     def update_item_page_data(site, file, data)
-      path = file.gsub('_tmp/content/_data/', '').gsub('.yml', '.md')
+      path = file.gsub('_data/', '').gsub('.yml', '.md')
       page = site.pages.detect { |page| page.path == path }
       page.data.merge! data
 
-      unless file == '_tmp/content/_data/index.yml'
+      unless file == '_data/index.yml'
         resource = file.match(/_data\/(?<resource>.+)\//)[1]
         page.data['parent_name'] = resource
         page.data['parent_url'] = "/#{resource}"
@@ -56,15 +33,12 @@ module Jekyll
 
     def update_list_page_data(site, file, data)
       resource = file.match(/_data\/(?<resource>.+)\//)[1]
+      page.data['resource_name'] = resource
+
       path = resource + '/index.md'
       page = site.pages.detect { |page| page.path == path }
-      page.data[resource] = [] if page.data[resource].nil?
-      page.data[resource].push data
-
       page.data['items'] = [] if page.data['items'].nil?
       page.data['items'].push data
-
-      page.data['resource_name'] = resource
     end
   end
 end
